6721818cafce7927522362dc83cd4eee9e3e522e
Mariahk <mariahkaytlene@gmail.com>
2018-08-31 13:14:42 +0530

    setup production webpack build

---
+++ readme.md
@@ -0,0 +1,8 @@
+#Git Commands
+
+
+git init-create new git repo
+git status-view changes
+git add add files to staging area
+gi commit-create a new commit for files in staging area
+git log-view recent commits
--- src/app.js
+++ src/app.js
@@ -8,19 +8,13 @@ import getVisibleExpenses from './selectors/expenses'
 import {addExpense} from './actions/expenses';
 import {setTextFilter} from './actions/filters';
 import { Provider } from 'react-redux';
+import 'react-dates/lib/css/_datepicker.css'; 
 
 
 const store=ConfigureStore();
 
 
-    
-
-
-
-
-
-
-    const jsx = (
+const jsx = (
       < Provider store={store}>  
         <AppRouter />
        </ Provider > 
